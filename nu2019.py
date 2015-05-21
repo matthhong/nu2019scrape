676ef9fccf883daeac80dae48a56fc5258e27077
Matt Hong <mhong19414@gmail.com>
2015-05-21 13:44:03 -0500

    Mechanize successfully queries and returns result from directory

--- nu2019.py
+++ nu2019.py
@@ -1,10 +1,17 @@
 import mechanize
+from bs4 import BeautifulSoup as bs
 
 br = mechanize.Browser()
 br.set_handle_robots(False)
 
 br.open('http://directory.northwestern.edu/?a=1')
 
+br.select_form('phadv')
+br.form['name'] = 'Matt Hong'
+br.form['affiliations'] = ['student']
+
+br.submit()
+
 response = br.response()
 
-print response.geturl()
+print response.read()
