3608d98fb0bb9c0202a3f72b88dbcbe8fb27639f
Matt Hong <mhong19414@gmail.com>
2015-05-23 18:35:49 -0500

    adds function to load nickname to name dictionary from a csv file

--- nu2019.py
+++ nu2019.py
@@ -1,17 +1,33 @@
+from collections import defaultdict as ddict
+import csv
 import mechanize
 from bs4 import BeautifulSoup as bs
 
-br = mechanize.Browser()
-br.set_handle_robots(False)
+def searchName(firstName, lastName):
+	br = mechanize.Browser()
+	br.set_handle_robots(False)
 
-br.open('http://directory.northwestern.edu/?a=1')
+	br.open('http://directory.northwestern.edu/?a=1')
 
-br.select_form('phadv')
-br.form['name'] = 'Matt Hong'
-br.form['affiliations'] = ['student']
+	br.select_form('phadv')
+	br.form['name'] = 'Matt Hong'
+	br.form['affiliations'] = ['student']
 
-br.submit()
+	br.submit()
 
-response = br.response()
+	response = br.response()
 
-print response.read()
+	print response.read()
+
+def getNameDict(filename):
+	nameDict = ddict(list)
+
+	with open(filename) as infile:
+		reader = csv.reader(infile, delimiter = ',')
+		for row in reader:
+			name = row[0]
+			for nickname in row[1:]:
+				nameDict[nickname].append(name)
+
+	return nameDict
+
